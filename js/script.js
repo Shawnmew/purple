69a48c7f4e8a0738d99c98a7455158671a9a8ae5
Shawnmew <144498256+Shawnmew@users.noreply.github.com>
2023-09-21 09:10:26 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.onscroll = () =>{
     background: '#fff url(/images/trees.png)',
     backdrop: `
       rgba(0,0,123,0.4)
-      url("images/logo.ico")
+      url("logo.ico")
       center top
       no-repeat
     `
